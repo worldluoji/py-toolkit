da361238a8232260257b025e5d77cdf6b7f0f257
worldluoji <qq1132416@outlook.com>
2023-11-06 14:43:03 +0800

    feat: support add font styles to paragraph

--- src/word/word_operator.py
+++ src/word/word_operator.py
@@ -1,5 +1,6 @@
 from docx import Document
 from docx.shared import RGBColor
+from docx.shared import Pt
 from enum import Enum  
 
 # https://python-docx.readthedocs.io/en/latest/#
@@ -9,6 +10,22 @@ class RETUENED_STATUS(Enum):
     NOT_CHANGED = 1
     FAIL_TO_SAVE = 2
 
+
+default_styles = {'font': {'name': '宋体', 'color': RGBColor(0,0,0), 'bold': False, 'underline': False, 'size': Pt(12)}}
+
+'''set the paragraph styles
+    :param para: the paragraph style object to be set
+    :param styles: a dict accroding to the reference: https://python-docx.readthedocs.io/en/latest/api/style.html#docx.styles.style.ParagraphStyle
+'''
+def set_paragraph_styles(para_style, styles):
+    if styles['font'] is None:
+        return
+    para_style.font.name = styles['font']['name'] if styles['font']['name'] is not None else default_styles['font']['name']
+    para_style.font.color.rgb = styles['font']['color'] if styles['font']['color'] is not None else default_styles['font']['color']
+    para_style.font.bold = styles['font']['bold'] if styles['font']['bold'] is not None else default_styles['font']['bold']
+    para_style.font.underline = styles['font']['underline'] if styles['font']['underline'] is not None else default_styles['font']['underline']
+    para_style.font.size = styles['font']['size'] if styles['font']['size'] is not None else default_styles['font']['size']
+
 '''add content in end of the doc
    :param doc: the word document to open
    :param content: the content to be insert
@@ -16,15 +33,9 @@ class RETUENED_STATUS(Enum):
    :param underline: if content with underline
    :param save_to: indicates where to save the modified doc
 '''
-def add_to_end(doc, content, font='宋体', underline = False, color='', save_to= ''):
+def add_to_end(doc, content, save_to= '', styles=default_styles):
     para = doc.add_paragraph().add_run(content)
-    # set font style
-    para.font.name = font
-    # set underline
-    para.font.underline = underline
-    # set color
-    if color != '':
-        para.font.color.rgb = color # RGBColor(255,128,128)
+    set_paragraph_styles(para, styles)
     
     if (save_to is not None) and len(save_to) > 0:
         try:
@@ -40,16 +51,17 @@ def add_to_end(doc, content, font='宋体', underline = False, color='', save_to
    :param keyword: the content to be insert before keyword
    :param content: the content to be insert
    :param stop: when found the first keyword then break if stop = True
-   :param font: the content's font style
-   :param underline: if content with underline
    :param save_to: indicates where to save the modified doc
+   :param styles: a dict accroding to the reference: https://python-docx.readthedocs.io/en/latest/api/style.html#docx.styles.style.ParagraphStyle
+   :return RETUENED_STATUS
 '''
-def add_before_text(doc, keyword, content, stop = True, font='宋体', underline = False, color='', save_to= ''):
+def add_before_text(doc, keyword, content, stop = True, save_to= '', styles = default_styles):
     ret = RETUENED_STATUS.NOT_CHANGED.value
     for para in doc.paragraphs:
         if para.text == keyword:
             para.insert_paragraph_before(content)
             ret = RETUENED_STATUS.SUCCESS.value
+            set_paragraph_styles(para.style, styles)
             if stop:
                 break
     
@@ -63,12 +75,22 @@ def add_before_text(doc, keyword, content, stop = True, font='宋体', underline
     return ret
 
 
-def replace_para(doc, keyword, content, stop = True, underline = False, color='', save_to= ''):
+'''replace paragraph's content which content is equal to text
+   :param doc: the word document to open
+   :param keyword: the content to be insert before keyword
+   :param content: the content to be insert
+   :param stop: when found the first keyword then break if stop = True
+   :param save_to: indicates where to save the modified doc
+   :param styles: a dict accroding to the reference: https://python-docx.readthedocs.io/en/latest/api/style.html#docx.styles.style.ParagraphStyle
+   :return RETUENED_STATUS
+'''
+def replace_para(doc, keyword, content, stop = True, save_to= '', styles = default_styles):
     ret = RETUENED_STATUS.NOT_CHANGED.value
     for para in doc.paragraphs:
         if para.text == keyword:
             para.text = content
             ret = RETUENED_STATUS.SUCCESS.value
+            set_paragraph_styles(para.style, styles)
             if stop:
                 break
         
--- test/word_operator_test/test_replace_para.py
+++ test/word_operator_test/test_replace_para.py
@@ -7,7 +7,7 @@ import os
 def test_replace_para():
     current_file_dir = os.path.dirname(os.path.abspath(__file__))
     doc = Document(os.path.join(current_file_dir, 'data', 'demo.docx'))
-    TEXT = 'Change leve 1'
+    TEXT = 'Change 级别 1'
     ret = word_operator.replace_para(doc, 'Heading, level 1', TEXT, save_to="text.docx")
     os.remove("text.docx")
     assert ret == 0
